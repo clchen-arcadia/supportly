89a1d83b0939b1a1571d0640454ab194fc2784f0
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-20 14:34:10 -0700

    Add User model

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,26 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 
+class User(db.Model):
+    """
+    User class.
+    """
+
+    __tablename__ = "users"
+
+    email = db.Column(db.Text, primary_key=True, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=True)
+
+    def to_dict(self):
+        """Serializer user to dict"""
+
+        return {
+            "email": self.email,
+            "isAdmin": self.is_admin,
+        }
+
+
 def connect_db(app):
     """
     Connect Flask app to the database.
