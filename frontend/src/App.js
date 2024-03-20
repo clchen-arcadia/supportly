a04b04183c5d74f8e8a12070106b9e9adc13cdac
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-20 14:10:08 -0700

    Render TicketForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,28 @@
 
 import { Typography } from "@mui/material/";
 
+import TicketForm from "./components/TicketForm";
+import SupportlyApi from "./Api";
+
 
 function App() {
+  function onTicketFormSubmit(formData) {
+    console.log(`submitted ticket form with formData=`, formData);
+  }
+
+
   return (
     <>
+      <nav>
+        test
+      </nav>
       <Typography variant="h4">
         Welcome to Supportly
       </Typography>
+
+      <TicketForm
+        onSubmit={onTicketFormSubmit}
+      />
     </>
   );
 }
