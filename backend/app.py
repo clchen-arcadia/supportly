84272c570f8536b8e7ed201d152ef35040023b86
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-20 14:30:03 -0700

    Configure app and connect db

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,20 @@
-from flask import Flask
+import os
+from flask import (
+    Flask,
+    request,
+    g,
+    jsonify,
+)
+
+from models import db, connect_db
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://")
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+
+connect_db(app)
