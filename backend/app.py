ac1c3da9fe710cdf7ebe35298b25a072f0343a4a
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-20 15:25:53 -0700

    Create route for new_tickets

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,11 @@ from flask import (
     g,
     jsonify,
 )
+from werkzeug.datastructures import MultiDict
+from sqlalchemy.exc import IntegrityError
 
-from models import db, connect_db
+from models import db, connect_db, Ticket
+from forms import TicketSubmit
 
 app = Flask(__name__)
 
@@ -18,3 +21,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 
 connect_db(app)
+
+
+@app.route('/tickets', methods=["POST"])
+def new_ticket():
+    """
+    Create new ticket
+    """
+
+    data = MultiDict(mapping=request.json)
+    form = TicketSubmit(data)
+
+    if form.validate():
+        client_name = form["client_name"]
+        client_email = form["client_email"]
+        description = form["description"]
+
+        try:
+            Ticket.create_new_ticket(
+                client_name=client_name,
+                client_email=client_email,
+                description=description,
+            )
+
+            db.session.commit()
+
+        except IntegrityError as e:
+            return jsonify({'errors': e.__repr__()}), 400
+
+        return jsonify({'success': 'New ticket submitted'}), 201
+
+    return jsonify(errors=form.errors), 400
