327cc7f0182a1fd61f4ebd351ccb20d08b214236
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-21 01:30:30 -0700

    Write ensure_logged_in

--- backend/middleware.py
+++ backend/middleware.py
@@ -12,3 +12,13 @@ def ensure_admin(func):
         else:
             return jsonify({"error": "User not authorized."}), 401
     return validate_admin
+
+
+def ensure_logged_in(func):
+    @wraps(func)
+    def validate_logged_in(*args, **kwargs):
+        if (g.user is not None):
+            return func(*args, **kwargs)
+        else:
+            return jsonify({"error": "User not authorized."}), 401
+    return validate_logged_in
