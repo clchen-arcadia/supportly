f0238b11250c5544439add21eb6e820b9fe2f471
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-20 22:58:12 -0700

    Test render signup and login forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,43 @@
 
 import { Typography } from "@mui/material/";
 
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 import TicketForm from "./components/TicketForm";
 import SupportlyApi from "./Api";
 
 
 function App() {
   async function onTicketFormSubmit(ticketFormData) {
-    // debugger;
     const response = await SupportlyApi.submitTicket(ticketFormData);
+    console.debug("response= ", response);
 
     if (response.ok) {
       alert(`Success! Ticket created.`);
     } else {
       alert(`Failure! Problem creating ticket.`);
+    }
+  }
+
+  async function onSignupFormSubmit(signupFormData) {
+    const response = await SupportlyApi.signupUser(signupFormData);
+    console.debug("response= ", response);
+
+    if (response.ok) {
+      alert(`Successfully created user.`);
+    } else {
+      alert(`Failed to create user.`);
+    }
+  }
+
+  async function onLoginFormSubmit(loginFormData) {
+    const response = await SupportlyApi.loginUser(loginFormData);
+    console.debug("response= ", response);
+
+    if (response.ok) {
+      alert(`Successfully logged in user.`);
+    } else {
+      alert(`Failed to log in user.`);
     }
   }
 
@@ -30,6 +54,14 @@ function App() {
 
       <TicketForm
         onSubmit={onTicketFormSubmit}
+      />
+
+      <SignupForm
+        onSubmit={onSignupFormSubmit}
+      />
+
+      <LoginForm
+        onSubmit={onLoginFormSubmit}
       />
     </>
   );
