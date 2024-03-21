2efad5ed613b0cee7286e1e1ecdca2864c030d6f
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-21 12:59:35 -0700

    Add simple header to admin home

--- frontend/src/components/HomepageAdmin.js
+++ frontend/src/components/HomepageAdmin.js
@@ -1,7 +1,17 @@
+import { Typography } from "@mui/material";
 
 
 function HomepageAdmin() {
 
+  return (
+    <Typography
+      textAlign='center'
+      variant='h4'
+      sx={{ m: 2 }}
+    >
+      Welcome, Admin, to Supportly
+    </Typography>
+  );
 }
 
 export default HomepageAdmin;
