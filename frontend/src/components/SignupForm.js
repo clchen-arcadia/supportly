c721ff0f043cefa8c3dd85cb2d2ea2bdbd636a12
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-20 22:57:36 -0700

    Add form elements

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,7 +1,76 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { useState } from 'react';
 
 
 function SignupForm({ onSubmit }) {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    is_admin: true,
+  });
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((fData) => ({
+      ...fData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onSubmit(formData);
+  }
+
+  const inputStyles = {};
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      <Box>
+        <Box>
+          <label htmlFor='email'>
+            <span style={{ fontWeight: 'bold' }}>
+              Email</span><span style={{ color: 'red' }}>*</span>
+            <br />
+            <input
+              name='email'
+              type='email'
+              value={formData.email}
+              onChange={handleChange}
+              style={inputStyles}
+              id='email'
+            />
+          </label>
+        </Box>
+
+        <Box>
+          <label htmlFor='password'>
+            <span style={{ fontWeight: 'bold' }}>
+              Password</span><span style={{ color: 'red' }}>*</span>
+            <br />
+            <input
+              name='password'
+              type='password'
+              value={formData.password}
+              onChange={handleChange}
+              style={inputStyles}
+              id='password'
+            />
+          </label>
+        </Box>
+
+        <Button
+          variant='contained'
+          type='submit'
+        >
+          Signup
+        </Button>
+      </Box>
+    </form>
+
+  );
 
 }
 
