a15cb770b838837187497eb67a1a52b741557234
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-21 17:31:11 -0700

    Refactor api to axios

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,80 +1,51 @@
+import axios from "axios";
 
 class SupportlyApi {
 
   static token = localStorage.getItem("supportly-token");
-  static baseUrl = process.env.REACT_APP_BASE_API_URL || "http://localhost:5001/";
+  static baseUrl = process.env.REACT_APP_BASE_API_URL || "http://localhost:5001";
 
-  static async fetchResponse(endpoint, method, body = {}) {
-    console.debug("API Call:", endpoint, method, body);
+  static async request(endpoint, data = {}, method = 'GET') {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${this.baseUrl}/${endpoint}`;
+    const headers = { token: this.token };
+
+    const params = (method === 'GET')
+      ? data
+      : {};
 
     try {
-      const response = await fetch(
-        this.baseUrl + endpoint,
-        {
-          method,
-          body,
-          headers: this.getHeaders(),
-          cache: "no-cache",
-        }
-      );
-      if (response.ok) {
-        return [true, response];
-      } else {
-        return [false, response];
-      }
+      return (await axios({ url, method, data, params, headers })).data;
     } catch (error) {
       console.error("API ERROR: ", error);
-      return [false, "Server error"];
+      throw error;
     }
-  }
-
-  static getHeaders(hasAuth = true) {
-    const headers = {
-      "Content-Type": "application/json"
-    };
-    if (hasAuth) {
-      headers['token'] = this.token;
-    }
-    return headers;
   }
 
   static async signupUser(signupFormData) {
-    const [success, response] = await this.fetchResponse('signup/', 'POST', signupFormData);
-    if (success) {
-      return [true, response.token];
-    } else {
-      return [false, Array.isArray(response.errors) ? response.errors.join(' ') : response.errors];
-    }
+    const res = await this.request(`signup/`, signupFormData, 'POST');
+    return res.token;
   }
 
   static async loginUser(loginFormData) {
-    const [success, response] = await this.fetchResponse('login/', 'POST', loginFormData);
-    if (success) {
-      return [true, response.token];
-    } else {
-      return [false, Array.isArray(response.errors) ? response.errors.join(' ') : response.errors];
-    }
+    const res = await this.request(`login/`, loginFormData, 'POST');
+    return res.token;
   }
 
   static async submitTicket(ticketSubmitData) {
-    const [success, response] = await this.fetchResponse('tickets/', 'POST', ticketSubmitData);
-
-    if (success) {
-      return [true, response.message];
-    } else {
-      return [false, Array.isArray(response.errors) ? response.errors.join(' ') : response.errors];
-    }
-
+    const res = await this.request(`tickets/`, ticketSubmitData, 'POST');
+    return res.message;
   }
 
   static async getCurrentUser(id) {
-    const [success, response] = await this.fetchResponse(`users/${id}`, 'GET');
+    const res = await this.request(`users/${id}`);
+    return res.user;
+  }
 
-    if (success) {
-      return [true, response.user];
-    } else {
-      return [false, Array.isArray(response.errors) ? response.errors.join(' ') : response.errors];
-    }
+  static async getTickets() {
+    const res = await this.request(`tickets/`);
+    return res.tickets;
   }
 }
 
