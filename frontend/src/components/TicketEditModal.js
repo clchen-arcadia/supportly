7c9914f4bb4a475aac327b044ea8758d2f915b61
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-21 19:09:49 -0700

    Add styles

--- frontend/src/components/TicketEditModal.js
+++ frontend/src/components/TicketEditModal.js
@@ -13,31 +13,57 @@ function TicketEditModal({ ticket, closeDrawer }) {
           justifyContent: 'end'
         }}
         onClick={closeDrawer}
+        m={1}
       >
         Close X
       </Box>
-      <Typography>
-        Viewing ticket id: {ticket.id}
-      </Typography>
 
-      <Typography>
-        ticket description: {ticket.description}
-      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
 
-      <Typography>
-        client name: {ticket.clientName}
-      </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            width: '40%',
+          }}
+          my={3}
+          mx={8}
+        >
 
-      <Typography>
-        client email: {ticket.clientEmail}
-      </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Ticket ID:</span> {ticket.id}
+          </Typography>
 
-      <Typography>
-        You can update the status, or send an email response to the ticket submitter
-      </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Description:</span> {ticket.description}
+          </Typography>
 
-      <TicketResponseForm ticketId={ticket.id} />
-      <TicketUpdateForm ticketId={ticket.id} />
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Client Name:</span> {ticket.clientName}
+          </Typography>
+
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Client Email:</span> {ticket.clientEmail}
+          </Typography>
+
+          <Typography>
+            You can update the status, or send an email response to the ticket submitter
+          </Typography>
+
+          <TicketResponseForm ticketId={ticket.id} />
+          <TicketUpdateForm ticketId={ticket.id} />
+        </Box>
+      </Box>
+
+
+
     </>
   );
 }
