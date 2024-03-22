9a62650262b96bbfccd194d5c797dc989bbadf76
clchen-arcadia <clchen.arcadia@gmail.com>
2024-03-21 17:32:06 -0700

    Add tickets call and render on mount
    * Also add drawer for drill down per ticket

--- frontend/src/components/HomepageAdmin.js
+++ frontend/src/components/HomepageAdmin.js
@@ -1,16 +1,87 @@
-import { Typography } from "@mui/material";
-
+import { useState, useEffect } from "react";
+import SupportlyApi from "../Api";
+import { Drawer, Typography, Box } from "@mui/material";
+import TicketCard from "./TicketCard";
+import TicketEditModal from "./TicketEditModal";
 
 function HomepageAdmin() {
 
+  const [pageData, setPageData] = useState({
+    isLoading: true,
+    tickets: [],
+  });
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [currTicket, setCurrTicker] = useState(null);
+
+  console.log("HomepageAdmin rendered with", "pageData=", pageData, "drawerOpen=", drawerOpen, "currTicket=", currTicket);
+
+  useEffect(
+    function loadTicketsOnMount() {
+
+      async function getAllTickets() {
+        const tickets = await SupportlyApi.getTickets();
+
+        setPageData({
+          isLoading: false,
+          tickets: tickets,
+        });
+
+
+      }
+      getAllTickets();
+    },
+    []
+  );
+
+  function onClickTicket(ticket) {
+    return () => {
+      setCurrTicker(ticket);
+      setDrawerOpen(true);
+    };
+  }
+
+
   return (
-    <Typography
-      textAlign='center'
-      variant='h4'
-      sx={{ m: 2 }}
-    >
-      Welcome, Admin, to Supportly
-    </Typography>
+    <>
+      <Typography
+        textAlign='center'
+        variant='h4'
+        sx={{ m: 2 }}
+      >
+        Welcome, Admin, to Supportly
+      </Typography>
+
+      <Typography
+        textAlign='center'
+        sx={{ m: 2 }}
+      >
+        As an admin, you can view tickets and update their status.
+      </Typography>
+
+      <Box>
+        {
+          pageData.isLoading
+            ? <Typography textAlign='center'>Loading tickets...</Typography>
+            : pageData.tickets.map(
+              (t) => {
+                return <TicketCard
+                  key={t.id}
+                  ticket={t}
+                  onClick={onClickTicket(t)}
+                />;
+              })
+        }
+      </Box>
+
+      <Drawer
+        anchor="bottom"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <TicketEditModal ticket={currTicket} closeDrawer={() => setDrawerOpen(false)} />
+      </Drawer>
+    </>
   );
 }
 
