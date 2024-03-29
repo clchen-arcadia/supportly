20793fef20dbc7ebdd654b581500042da0fa6054
Christopher Chen <clchen.arcadia@gmail.com>
2024-03-29 16:04:40 -0700

    Create LoadingScreen

---
+++ frontend/src/components/LoadingScreen.js
@@ -0,0 +1,24 @@
+import { Typography, CircularProgress, Box } from '@mui/material';
+
+function LoadingScreen() {
+  return (
+    <Box
+      m={2}
+      mt={5}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+
+      <Typography
+        mx={2}
+        variant="h4">
+        Loading...
+      </Typography>
+      <CircularProgress mx={2} />
+    </Box>
+  );
+}
+
+export default LoadingScreen;
