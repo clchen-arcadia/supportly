58de601367d90a8f7e3390bffb5c6068218f9c6a
Christopher Chen <clchen.arcadia@gmail.com>
2024-03-30 17:14:53 -0700

    Format code

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -5,14 +5,16 @@ import LoginForm from "./LoginForm";
 function LoginPage({ handleLogin }) {
   return (
     <>
+
       <Typography
         variant="h5"
         textAlign='center'
-        sx={{ m: 2, mt: 4, }}
-      >
+        sx={{ m: 2, mt: 4, }}>
         Log in as admin
       </Typography>
+
       <LoginForm handleLogin={handleLogin} />
+
     </>
   );
 }
