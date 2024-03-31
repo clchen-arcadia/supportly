99ba71094216b22660bacc71a3026788cd197160
Christopher Chen <clchen.arcadia@gmail.com>
2024-03-30 20:05:59 -0700

    Add controller pattern

--- backend/views/auth.py
+++ backend/views/auth.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.datastructures import MultiDict
 from forms import UserLogin, UserSignup
-from sqlalchemy.exc import IntegrityError
 from models import db, User
 from middleware import ensure_logged_in
-from token_helpers import create_token
+from services import get_form_errors
+from controllers import signup_new_user, login_user
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @bp.route("/signup/", methods=["POST"])
-def signup():
+def signup_route():
     """Handle user signup"""
 
     data = MultiDict(mapping=request.json)
@@ -20,25 +20,13 @@ def signup():
         email = form.data["email"]
         password = form.data["password"]
         is_admin = form.data["is_admin"]
-
-        try:
-            user = User.signup(email, password, is_admin)
-            db.session.commit()
-            return jsonify({"token": create_token(user)}), 201
-        except IntegrityError as e:
-            print("ERR: ", e)
-            return jsonify({"errors": "Email already exists."}), 400
-
+        return signup_new_user(email, password, is_admin)
     else:
-        messages = []
-        for err in form.errors:
-            joined_messages = " ".join(form.errors[err])
-            messages.append(f"{err}: {joined_messages}")
-        return jsonify({"errors": messages}), 400
+        return jsonify({"errors": get_form_errors(form)}), 400
 
 
 @bp.route("/login/", methods=["POST"])
-def login():
+def login_route():
     """Handle user login"""
 
     data = MultiDict(mapping=request.json)
@@ -47,23 +35,15 @@ def login():
     if form.validate():
         email = form.data["email"]
         password = form.data["password"]
-        user = User.login(email, password)
-        if user:
-            return jsonify({"token": create_token(user)}), 200
-        else:
-            return jsonify({"error": "Invalid email/password"}), 400
-
+        return login_user(email, password)
     else:
-        messages = []
-        for err in form.errors:
-            joined_messages = " ".join(form.errors[err])
-            messages.append(f"{err}: {joined_messages}")
-        return jsonify({"errors": messages}), 400
+        return jsonify({"errors": get_form_errors(form)}), 400
 
 
 @bp.route("/users/<user_id>/", methods=["GET"])
 @ensure_logged_in
-def get_user(user_id):
+def get_user_route(user_id):
     """Get user by id"""
+
     user = User.query.get_or_404(user_id)
     return jsonify({"user": user.to_dict()}), 200
--- backend/views/tickets.py
+++ backend/views/tickets.py
@@ -1,27 +1,24 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.datastructures import MultiDict
 from forms import TicketNewForm, TicketStatusPatchForm, TicketResponseForm
-from sqlalchemy.exc import IntegrityError
-from models import db, Ticket, Status
+from models import Ticket
 from middleware import ensure_admin
+from services import get_form_errors, send_ticket_dummy_email
+from controllers import get_all_tickets, create_new_ticket, patch_ticket_status
 
 bp = Blueprint('tickets', __name__, url_prefix='/tickets')
 
 
 @bp.route("/", methods=["GET"])
 @ensure_admin
-def get_all_tickets():
-    """Get all tickets by descending id number"""
+def get_tickets_route():
+    """Get all tickets"""
 
-    tickets = []
-    for ticket in Ticket.query.order_by(Ticket.id.desc()).all():
-        tickets.append(ticket.to_dict())
-
-    return jsonify({"tickets": tickets}), 200
+    return jsonify({"tickets": get_all_tickets()}), 200
 
 
 @bp.route("/", methods=["POST"])
-def new_ticket():
+def post_ticket_route():
     """Create new ticket"""
 
     data = MultiDict(mapping=request.json)
@@ -32,28 +29,14 @@ def new_ticket():
         client_email = form.data["email"]
         description = form.data["description"]
 
-        try:
-            Ticket.create_new_ticket(
-                client_name=client_name,
-                client_email=client_email,
-                description=description,
-            )
-            db.session.commit()
-        except IntegrityError as e:
-            return jsonify({"errors": e.__repr__()}), 400
-
-        return jsonify({"message": "New ticket submitted"}), 201
+        return create_new_ticket(client_name, client_email, description)
     else:
-        messages = []
-        for err in form.errors:
-            joined_messages = " ".join(form.errors[err])
-            messages.append(f"{err}: {joined_messages}")
-        return jsonify({"errors": messages}), 400
+        return jsonify({"errors": get_form_errors(form)}), 400
 
 
 @bp.route("/<ticket_id>/", methods=["PATCH"])
 @ensure_admin
-def update_ticket_status(ticket_id):
+def patch_ticket_route(ticket_id):
     """Update ticket status"""
 
     ticket = Ticket.query.get_or_404(ticket_id)
@@ -62,45 +45,23 @@ def update_ticket_status(ticket_id):
 
     if form.validate():
         new_status = form.data["new_status"]
-        status = Status.query.filter_by(status_name=new_status).first()
-        if not status:
-            return jsonify({"error": "No such status."}), 404
-        try:
-            ticket.status_name = status.status_name
-            db.session.commit()
-        except IntegrityError as e:
-            return jsonify({"errors": e.__repr__()}), 400
-
-        return jsonify({"message": "Successfully updated ticket"}), 200
+        return patch_ticket_status(ticket, new_status)
     else:
-        messages = []
-        for err in form.errors:
-            joined_messages = " ".join(form.errors[err])
-            messages.append(f"{err}: {joined_messages}")
-        return jsonify({"errors": messages}), 400
+        return jsonify({"errors": get_form_errors(form)}), 400
 
 
 @bp.route("/<ticket_id>/email/", methods=["POST"])
 @ensure_admin
-def send_ticket_response(ticket_id):
+def ticket_response_route(ticket_id):
     """Simulate sending an email to respond to a ticket"""
 
     ticket = Ticket.query.get_or_404(ticket_id)
     data = MultiDict(mapping=request.json)
     form = TicketResponseForm(data)
+
     if form.validate():
         response = form.data["response"]
-
-        print("SENDING EMAIL", flush=True)
-        print("==========================================", flush=True)
-        print("mailto: ", ticket.client_email, flush=True)
-        print("subject: ", f"Regarding ticket: {ticket.id}", flush=True)
-        print("body: ", f"Dear {ticket.client_name}. {response}", flush=True)
-
+        send_ticket_dummy_email(ticket, response)
         return jsonify({"message": "Successfully sent response"}), 200
     else:
-        messages = []
-        for err in form.errors:
-            joined_messages = " ".join(form.errors[err])
-            messages.append(f"{err}: {joined_messages}")
-        return jsonify({"errors": messages}), 400
+        return jsonify({"errors": get_form_errors(form)}), 400
